fab49bffc274bdbe3e01a969c7fc3b4a7b3b4361
Edward D'Souza <edward.dsouza@polarmobile.com>
2016-06-14 21:22:41 -0400

    Test importing jQuery as ES6 module.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import * as glass from './glass';
+import $ from '../node_modules/jquery/dist/jquery.js'
 import * as three from '../node_modules/three/build/three.js'
 
 console.log(glass.glassType);
 console.log(three.Scene());
+
+function run() {
+    var $container = $('#container');
+    console.log($container);
+}
+
+run();
