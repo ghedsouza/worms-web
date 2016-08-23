39738ecf59efdcdcf938688e7645f33ab87c77c7
kha0 <cha0@outlook.com>
2016-08-22 22:23:05 -0400

    Adding norm, disp, spec maps

--- src/ground.js
+++ src/ground.js
@@ -5,7 +5,20 @@ export const surface = function() {
 	var floorTexture = new THREE.ImageUtils.loadTexture( 'images/dirt-seamless.jpg' );
 	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
 	floorTexture.repeat.set(100, 100);
-	const basicMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, side: THREE.FrontSide });
-	const floorGeometry = new THREE.PlaneGeometry(800, 600);
+	
+	var floorNorm = new THREE.ImageUtils.loadTexture( 'images/dirt-seamless_NRM.png' );
+	floorNorm.wrapS = floorNorm.wrapT = THREE.RepeatWrapping;
+	floorNorm.repeat.set(100, 100);
+	
+	var floorSpec = new THREE.ImageUtils.loadTexture( 'images/dirt-seamless_SPEC.png' );
+	floorSpec.wrapS = floorSpec.wrapT = THREE.RepeatWrapping;
+	floorSpec.repeat.set(100, 100);
+	
+	var floorDisp = new THREE.ImageUtils.loadTexture( 'images/dirt-seamless_DISP.png' );
+	floorDisp.wrapS = floorDisp.wrapT = THREE.RepeatWrapping;
+	floorDisp.repeat.set(100, 100);
+	
+	const basicMaterial = new THREE.MeshPhongMaterial({ map: floorTexture, normalMap: floorNorm, specularMap: floorSpec, displacementMap: floorDisp, side: THREE.FrontSide });
+	const floorGeometry = new THREE.PlaneGeometry(1280, 1024);
     return new THREE.Mesh(floorGeometry, basicMaterial);
 };
